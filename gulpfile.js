83534e797a5d157c1dda9cac328da0f7a1f9a9e4
FoziljonYoqubjonov <foziljonyoqubjonov1@gmail.com>
2021-08-26 23:36:37 +0500

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,7 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch('app/scss/*.scss', gulp.parallel('sass'));
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
     gulp.watch('app/*.html', gulp.parallel('html'));
     gulp.watch('app/js/*.js', gulp.parallel('js'));
 });
